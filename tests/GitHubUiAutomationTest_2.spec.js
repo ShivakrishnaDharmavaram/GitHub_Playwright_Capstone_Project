d19972bfec8b96d31f811ae0a0b6460341651672
ShivakrishnaDharmavaram <shivakrishnadss1999@gmail.com>
2026-02-24 12:45:20 +0530

    Global_setup

---
+++ tests/GitHubUiAutomationTest_2.spec.js
@@ -0,0 +1,205 @@
+import {test, expect} from '@playwright/test';
+import {LoginPage} from '../pages/LoginPage';
+import {HomePage} from '../pages/HomePage';
+import {RepoPage} from '../pages/RepoPage';
+import {NewRepoPage} from '../pages/NewRepo';
+import {NewRepoDetails} from '../pages/NewRepoDetails';
+
+import NewFileDetails from '../files/NewFileDetails.json';
+
+import * as updateTestData from '../utils/testData.js';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+test.describe.configure({ mode: 'serial' });
+test.describe('GitHub Automation using Playwright ', () => {
+    let context;
+    let page;
+    const url = process.env.GITHUB_URL || "https://github.com/login";
+    const userId = process.env.GITHUB_USER;
+    const password = process.env.GITHUB_PASSWORD;
+
+    // Set up the browser context and page before all tests
+    test.beforeAll('TC-00- Setup Browser Context and Page', async ({ browser }) => {
+        context = await browser.newContext();
+        page = await context.newPage();
+    });
+
+    test.afterAll('Testcase to Close Browser Context', async () => {
+        await context.close();
+    });
+
+    test('Test-01-GitHub Login', async () => {
+        const loginPage = new LoginPage(page);
+
+        await loginPage.navigate(url);
+        await page.waitForLoadState('domcontentloaded');
+        await expect(page).toHaveTitle(/Sign in to GitHub/);
+
+        //Sign in with valid credentials
+        await loginPage.login(userId, password);
+
+        const currentUrl =await loginPage.getCurrentURL();
+        console.log("URL after login:", currentUrl);
+
+
+        const isLoggedIn = currentUrl === 'https://github.com/';
+        const isDeviceVerification = currentUrl.includes('/sessions/verified-device');
+
+        expect(isLoggedIn || isDeviceVerification).toBeTruthy();
+
+        // If device verification is required, handle it
+        if (isDeviceVerification) {
+            console.log("Device verification is required. Please complete the verification process manually.");
+            // Wait for the user to complete device verification
+            await page.waitForNavigation({ waitUntil: 'networkidle' });
+            const postVerificationUrl = await loginPage.getCurrentURL();
+            console.log("URL after device verification:", postVerificationUrl);
+            expect(postVerificationUrl).toBe('https://github.com/');
+        }   else {
+            console.log("Logged in successfully without device verification.");
+        }
+        await page.screenshot({ path: `screenshots/login_success_${Date.now()}.png`, fullPage: true });   
+        await page.waitForTimeout(2000);
+    });
+
+    test('Test-02-Verify the dashboard visibility', async () => {
+        const homePage = new HomePage(page);
+        // Check if the dashboard is visible after login
+        const isDashboardVisible = await homePage.isDashboardVisible();
+        console.log("Is the dashboard visible?", isDashboardVisible);
+        expect(isDashboardVisible).toBeTruthy();
+    });
+
+    test('Test-03-Verify that user is able to navigate to repositories page', async () => {
+        const homePage = new HomePage(page);
+        await homePage.navigateToRepositories();
+        const repositoriesUrl = await homePage.getCurrentURL();
+        console.log("URL after navigating to repositories:", repositoriesUrl);
+        expect(repositoriesUrl).toBe('https://github.com/repos');
+        await page.waitForLoadState('domcontentloaded');
+        await page.screenshot({ path: `screenshots/repositories_page_${Date.now()}.png`, fullPage: true });
+        await page.waitForTimeout(3000);
+    });
+
+    test('Test-04-Verify that user is able to navigate to My repositories page', async () => {
+        const repositoryName = `playwright-repo-${Date.now()}`;
+        const repoPage = new RepoPage(page);
+
+        // Navigate to My repositories page
+        await repoPage.navigateToMyRepositories();
+        const myRepositoriesUrl = await page.url();
+        console.log("URL after navigating to My repositories:", myRepositoriesUrl);
+        expect(myRepositoriesUrl).toBe('https://github.com/repos');
+        await page.waitForLoadState('domcontentloaded');
+        await page.screenshot({ path: `screenshots/my_repositories_page_${Date.now()}.png`, fullPage: true });
+        await page.waitForTimeout(3000);
+    });
+    test('Test-05-Verify that user is able to identify the mentioned repository in My repositories page', async () => {
+        const repoPage = new RepoPage(page);
+        // Check if the specific repository is visible in My repositories page
+        const isRepoVisible = await repoPage.isMyRepoVisible();
+        console.log("Is the specified repository visible?", isRepoVisible);
+        expect(isRepoVisible).toBeTruthy();
+        await page.waitForLoadState('domcontentloaded');
+    });
+
+    test('Test-06-Verify that user is able to navigate to create new repository page', async () => {
+        const repoPage = new RepoPage(page);
+        // Navigate to create new repository page
+        await repoPage.navigateToCreateNewRepository();
+        const newRepoUrl = await page.url();
+        console.log("URL after navigating to create new repository:", newRepoUrl);
+        expect(newRepoUrl).toBe('https://github.com/new');
+        await page.waitForLoadState('domcontentloaded');
+        await page.screenshot({ path: `screenshots/create_new_repository_page_${Date.now()}.png`, fullPage: true });
+        await page.waitForTimeout(3000);
+    });
+
+    test('Test-07- Verify that user is able to fill the details to create a new repository', async () => {
+        const newRepoPage = new NewRepoPage(page);
+        const repoData = updateTestData.createRepoPayload();
+
+        // Create a new repository
+        await newRepoPage.createNewRepository(repoData);
+        await page.waitForTimeout(2000);
+        await newRepoPage.selectPrivateVisibility();
+        await page.waitForTimeout(2000);
+        await newRepoPage.selectPublicVisibility();
+        await page.screenshot({ path: `screenshots/new_repository_details_entered_${Date.now()}.png`, fullPage: true });
+        await page.waitForTimeout(3000);
+
+        // Click on Create repository button to create the new repository
+        await newRepoPage.clickCreateRepository();
+        await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
+        const createdRepoUrl = await page.url();
+        console.log("URL after creating new repository:", createdRepoUrl);
+        await page.screenshot({ path: `screenshots/repository_created_${Date.now()}.png`, fullPage: true });
+        await page.waitForTimeout(3000);
+        expect(createdRepoUrl).toBe(`https://github.com/${userId}/${repoData.name}`);
+
+
+    });
+    test('Test-08-Verify that user is able to click on create new file hyperlink', async () => {
+        const newRepoDetails = new NewRepoDetails(page);
+
+        // Select the option to create README file
+        await newRepoDetails.selectNewFileCreation();
+        await page.waitForLoadState('domcontentloaded');
+        const readmeUrl = await page.url();
+        console.log("URL after selecting file creation:", readmeUrl);
+        await page.screenshot({ path: `screenshots/file_creation_page_${Date.now()}.png`, fullPage: true });
+        await page.waitForTimeout(3000);
+    });
+
+    test('Test-9-Verify that user is able to fill the details for the new file and commit changes', async () => {
+        const newRepoDetails = new NewRepoDetails(page);
+        const fileData = NewFileDetails;
+        
+        // Fill the file name and content for the new file
+        await newRepoDetails.fillFileDetails(fileData.fileName, fileData.description);
+        await page.waitForTimeout(2000);
+        await page.screenshot({ path: `screenshots/new_file_details_entered_${Date.now()}.png`, fullPage: true });
+        await page.waitForTimeout(3000);
+        
+        // Click on Commit changes button to create the new file
+        await newRepoDetails.clickCommitChanges();
+        await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
+        const repoUrl = await page.url();
+        console.log("URL after committing new file:", repoUrl);
+        expect(repoUrl).toContain(`/${fileData.fileName}`); //expect(repoUrl).toContain(`/${userId}/${repoData.name}`);
+        await page.screenshot({ path: `screenshots/file_created_${Date.now()}.png`, fullPage: true });
+        await page.waitForTimeout(3000);
+
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// To run the test, use the following command in the terminal:
+// npx playwright test tests/GitHubUiAutomationTest_2.spec.js --headed
